2a1101e2481168da14445eae89169a5f29deb741
Dennis-ux-ai <dennissabu444@gmail.com>
2025-02-04 07:19:15 +0530

    Create prog 4.c

---
+++ prog 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  float r,area,perimeter;
+  printf("Enter radius of circle");
+  scanf("%f" ,&r);
+  area = 3.14*r;
+  perimeter = 2*3.14*r;
+  printf("area of the cirle is %.2f\n",area);
+  printf("perimeter of the cirle is %.2f\n",perimeter);
+   
+}
