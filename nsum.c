a7ea037440dbb7c33ef3a85784dbbbe05d216336
Dennis-ux-ai <dennissabu444@gmail.com>
2025-03-02 09:53:31 +0530

    Add files via upload

---
+++ nsum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,sum=0;
+    printf("enter the number of elements");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter %d elements",n);
+    for(i=0;i<0;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+
+    }
+    printf("sum of all elements=%d",sum);
+}
